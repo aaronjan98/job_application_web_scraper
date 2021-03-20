3da8fe50fcabffca318c324d0031da42b72b3811
aaronjan98 <aaronjan98@gmail.com>
2021-03-20 00:10:22 -0700

    added readme

--- indeed.py
+++ indeed.py
@@ -1,12 +1,18 @@
-# For automating applying for jobs, I'm going to use the 'Brave Browser' and 'Selenium' as the driver
-# Selenium is a leading cloud-based testing platform which helps testers record their actions and export them as a reusable script.
-# see this resource for an example of web-scraping Indeed: https://www.youtube.com/watch?v=e-3DafD-c4w
-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import time
+
 PATH = "/usr/local/bin/chromedriver"
-options = Options()
-options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
-options.add_argument("start-maximized")
-driver = webdriver.Chrome(options=options, executable_path=PATH)
+browser_options = Options()
+browser_options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+browser_options.add_argument("start-maximized")
+
+driver = webdriver.Chrome(options=browser_options, executable_path=PATH)
+
 driver.get("https://aaronjanovitch.com")
+print(driver.title)
+
+time.sleep(5)
+
+driver.quit()
