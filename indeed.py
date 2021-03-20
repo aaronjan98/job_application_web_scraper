53ca45109ee5b0554d8edd614904277b14c9106b
aaronjan98 <aaronjan98@gmail.com>
2021-03-19 23:29:01 -0700

    added Brave binary path to ChromeOptions class provided by Selenium

--- indeed.py
+++ indeed.py
@@ -3,7 +3,10 @@
 # see this resource for an example of web-scraping Indeed: https://www.youtube.com/watch?v=e-3DafD-c4w
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 PATH = "/usr/local/bin/chromedriver"
-driver = webdriver.Brave(PATH)
-
+options = Options()
+options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+options.add_argument("start-maximized")
+driver = webdriver.Chrome(options=options, executable_path=PATH)
 driver.get("https://aaronjanovitch.com")
