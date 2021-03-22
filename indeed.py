07bacd30eabfdf709cf61683d576476bfc45a52e
aaronjan98 <aaronjan98@gmail.com>
2021-03-22 12:25:34 -0700

    going to try opening a current running browser session

--- indeed.py
+++ indeed.py
@@ -1,18 +1,61 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+# Retrieving login information from .env
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Accessing variables.
+email = os.getenv('LINKEDIN_EMAIL')
+password = os.getenv('LINKEDIN_PASSWORD')
+
+# Setting up Browser configurations
 PATH = "/usr/local/bin/chromedriver"
 browser_options = Options()
 browser_options.binary_location = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
 browser_options.add_argument("start-maximized")
-
 driver = webdriver.Chrome(options=browser_options, executable_path=PATH)
 
-driver.get("https://aaronjanovitch.com")
-print(driver.title)
+driver.get("https://www.indeed.com")
 
-time.sleep(5)
+try:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Sign in"))
+    ).click()
+except:
+    driver.quit()
 
-driver.quit()
+# Sign in
+# driver.find_element_by_link_text("Sign in").click()
+
+# check if this element exists first
+# email_input = driver.find_element_by_id("login-email-input")
+
+try:
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "login-email-input"))
+    ).send_keys(email)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "login-password-input"))
+    ).send_keys(password)
+    # check for captcha
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "checkbox"))
+    ).click()
+    # WebDriverWait(driver, 10).until(
+        # EC.presence_of_element_located((By.ID, "login-submit-button"))
+    # ).send_keys(Keys.RETURN)
+except:
+    driver.quit()
+
+# time.sleep(5)
+# driver.quit()
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # Automate Applying to Jobs
-- I'm going to use 'Brave Browser' and 'Selenium' as the driver
+I'm going to use 'Brave Browser' and 'Selenium' as the driver  
     Selenium is a leading cloud-based testing platform which helps testers record their actions and export them as a reusable script
 
